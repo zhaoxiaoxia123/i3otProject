fede952ec442702e6fcacf4214fa975563456568
zxx <540578223@qq.com>
2017-08-29 17:18:52 +0800

    cangku

--- src/app/+forms/add-inventory1/add-inventory1.component.ts
+++ src/app/+forms/add-inventory1/add-inventory1.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FadeInTop} from '../../shared/animations/fade-in-top.decorator';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Http} from '@angular/http';
+import {Router} from '@angular/router';
 
 @FadeInTop()
 @Component({
@@ -8,9 +11,47 @@ import {FadeInTop} from '../../shared/animations/fade-in-top.decorator';
 })
 export class AddInventory1Component implements OnInit {
 
-  constructor() { }
+  formModel : FormGroup;
+  constructor(
+      fb:FormBuilder,
+      private http:Http,
+      private router : Router
+  ) {
+    this.formModel = fb.group({
+      storehouse_name:['',[Validators.required,Validators.minLength(1)]],
+      storehouse_total_quantity:[''],
+      storehouse_status:[''],
+      storehouse_keeper:[''],
+      storehouse_phone:[''],
+      storehouse_notes:[''],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.http.post('/api/v1/addStorehouse',{
+      'storehouse_name':this.formModel.value['storehouse_name'],
+      'storehouse_total_quantity':this.formModel.value['storehouse_total_quantity'],
+      'storehouse_status':this.formModel.value['storehouse_status'],
+      'storehouse_keeper':this.formModel.value['storehouse_keeper'],
+      'storehouse_phone':this.formModel.value['storehouse_phone'],
+      'storehouse_notes':this.formModel.value['storehouse_notes']
+    }).subscribe(
+        (data)=>{
+          alert(JSON.parse(data['_body'])['msg']);
+          if(data['status'] == 200) {
+            this.router.navigateByUrl('/tables/inventory');
+          }
+        },
+        response => {
+          console.log('PATCH call in error', response);
+        },
+        () => {
+          console.log('The PATCH observable is now completed.');
+        }
+    );
   }
 
 }
