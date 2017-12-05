84351f70d9fc0294f8af6fe429331335136d94b2
MuXPhoenix <3427870036@qq.com>
2017-12-05 13:08:07 +0800

    dff

--- src/app/+dashboard/dynamic-wall/panel-staff/panel-staff.component.ts
+++ src/app/+dashboard/dynamic-wall/panel-staff/panel-staff.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-panel-staff',
@@ -6,10 +8,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel-staff.component.css']
 })
 export class PanelStaffComponent implements OnInit {
+    //方法1的 start
+    dataSource: Observable<any>;
+    products: Array<any> = [];
+    ////方法1的 end
+    chartOption;
+    seriesInfo: Array<any> = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http:Http) {
   }
+
+
+    ngOnInit() {
+            this.getSeriesInfo();
+
+    }
+    getSeriesInfo(){
+
+        this.chartOption = {tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+            legend: {
+                orient: 'vertical',
+                x: 'left',
+                data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+            },
+            series: [
+                {
+                    name:'访问来源',
+                    type:'pie',
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: '30',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
+                    data:[
+                        {value:1221, name:'直接访问'},
+                        {value:220, name:'邮件营销'},
+                        {value:234, name:'联盟广告'},
+                        {value:135, name:'视频广告'},
+                        {value:12, name:'搜索引擎'}
+                    ]
+                }
+            ]
+        }
+    }
 
 }
