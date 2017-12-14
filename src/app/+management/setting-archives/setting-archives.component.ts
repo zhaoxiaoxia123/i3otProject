5cb23bb3a5f9228abec387bbc58557567c4cd49e
MuXPhoenix <3427870036@qq.com>
2017-12-14 11:16:33 +0800

    1233

--- src/app/+management/setting-archives/setting-archives.component.ts
+++ src/app/+management/setting-archives/setting-archives.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting-archives.component.css']
 })
 export class SettingArchivesComponent implements OnInit {
+    public states: Array<any>;
+    public state: any = {
+        tabs: {
+            demo1: 0,
+            demo2: 'tab-r1',
+            demo3: 'hr1',
+            demo4: 'AA',
+            demo5: 'iss1',
+            demo6: 'l1',
+            demo7: 'tab1',
+            demo8: 'hb1',
+            demo9: 'A1',
+            demo10: 'is1'
+        },
+    };
 
   constructor() { }
 
