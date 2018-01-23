195d2a20e8a8394d9a87a33692be0f9c1f2c4037
MuXPhoenix <3427870036@qq.com>
2018-01-23 17:13:32 +0800

    255

--- src/app/+graphs/graphs-showcase.routing.ts
+++ src/app/+graphs/graphs-showcase.routing.ts
@@ -14,6 +14,9 @@ export const routes: Routes = [
     path: 'morris-charts',
     loadChildren: 'app/+graphs/+morris-charts/morris-charts.module#MorrisChartsModule'
   },
+    { path: 'tree-views',
+        loadChildren: 'app/+graphs/+tree-views/tree-views.module#TreeViewsModule',
+        data: {pageTitle: 'Tree Views'} },
   {
     path: 'sparklines',
     component: SparklinesComponent
