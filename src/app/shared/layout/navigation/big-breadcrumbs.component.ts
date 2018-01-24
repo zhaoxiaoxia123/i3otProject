8871c552cadc2a6679e4097dee0e13f88b778aa0
MuXPhoenix <3427870036@qq.com>
2018-01-24 14:17:42 +0800

    12555

--- src/app/shared/layout/navigation/big-breadcrumbs.component.ts
+++ src/app/shared/layout/navigation/big-breadcrumbs.component.ts
@@ -4,7 +4,7 @@ import {Component, OnInit, Input} from '@angular/core';
 
   selector: 'sa-big-breadcrumbs',
   template: `
-   <div><h1 class="page-title txt-color-blueDark">
+   <div class="pdl-13"><h1 class="page-title txt-color-blueDark">
    <i class="fa-fw fa fa-{{icon}}"></i>{{items[0]}}
    <span *ngFor="let item of items.slice(1)">> {{item}}</span>
 </h1></div>
