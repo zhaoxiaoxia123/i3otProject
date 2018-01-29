70c6f6591074fb1ea7c242c688f16ebed29bc2a3
zxx <540578223@qq.com>
2018-01-29 16:26:35 +0800

    xg\

--- src/app/+management/setting-departmentnew/setting-departmentnew.component.ts
+++ src/app/+management/setting-departmentnew/setting-departmentnew.component.ts
@@ -37,15 +37,17 @@ export class SettingDepartmentnewComponent implements OnInit {
 
     //左侧选中部门的id
     select_department_ids: Array<any> = [];
-        //左边展开和收起功能
+    //左边展开和收起功能
     showUl : number  = 1;
 
     //顶部启动 和无效是否启用显示
     editStatusDepartmentId : any = 0;
     isStatus : any = 0;
 
+    //处理批量
+    isAll : number = 0;
+
     customer_name : string = '';
-    department_id : number = 0;
     keyword:string = '';
     rollback_url : string = '/management/setting-departmentnew';
   constructor(
@@ -89,6 +91,16 @@ export class SettingDepartmentnewComponent implements OnInit {
         }
         if(department_id != 0){
             url += '&ids='+department_id;
+        }else{
+
+            let depart = '';
+            this.select_department_ids.forEach((val, idx, array) => {
+                if(val == true) {
+                    depart += idx + ',';
+                }
+            });
+
+            url += '&depart='+depart;
         }
         this.http.get(url)
             .map((res)=>res.json())
@@ -147,8 +159,8 @@ export class SettingDepartmentnewComponent implements OnInit {
         }else{
             this.check = true;
         }
-        // console.log(this.selects);
-        // console.log(this.check);
+        this.editStatusDepartmentId = 0;
+        this.isStatus = 0;
     }
 
     /**
@@ -159,7 +171,6 @@ export class SettingDepartmentnewComponent implements OnInit {
             .map((res)=>res.json())
             .subscribe((data)=>{
                 this.departmentDefault = data;
-                console.log(this.departmentDefault);
                 if(this.departmentDefault['status'] == 202){
                     alert(this.departmentDefault['msg']);
                     this.cookieStore.removeAll(this.rollback_url);
@@ -172,38 +183,40 @@ export class SettingDepartmentnewComponent implements OnInit {
      *  type ： （ edit ：修改部门  ；  detail  ： 部门详情）
      */
     detailDepartment(type:string){
-        let isAll = 0;
-        let department_id = 0;
-        this.selects.forEach((val, idx, array) => {
-            if(val == true) {
-                isAll += 1;
-                department_id = idx;
-            }
-        });
-        let msg = '';
-        if(isAll <= 0){
-            msg = '请选中要操作的信息，再点击此按钮！';
-        }else if(isAll > 1){
-            msg = '仅支持选择一条要操作的信息！';
-        }
-        if(msg != ''){
-            alert(msg);
+        if(this.isStatus == 0){
             return false;
         }
-
-        this.department_id = department_id;
+        // let isAll = 0;
+        // let department_id = 0;
+        // this.selects.forEach((val, idx, array) => {
+        //     if(val == true) {
+        //         isAll += 1;
+        //         department_id = idx;
+        //     }
+        // });
+        // let msg = '';
+        // if(isAll <= 0){
+        //     msg = '请选中要操作的信息，再点击此按钮！';
+        // }else if(isAll > 1){
+        //     msg = '仅支持选择一条要操作的信息！';
+        // }
+        // if(msg != ''){
+        //     alert(msg);
+        //     return false;
+        // }
+        // this.department_id = department_id;
         if(type == 'edit'){
             this.lgModal.show();
         }else{
             this.detailModal.show();
         }
-        this.http.get(this.globalService.getDomain()+'/api/v1/getDepartmentInfo?department_id='+department_id+'&type='+type+'&sid='+this.cookieStore.getCookie('sid'))
+        this.http.get(this.globalService.getDomain()+'/api/v1/getDepartmentInfo?department_id='+this.editStatusDepartmentId+'&type='+type+'&sid='+this.cookieStore.getCookie('sid'))
             .map((res)=>res.json())
             .subscribe((data)=>{
                 this.departmentInfo = data;
                 if(this.departmentInfo['status'] == 200 && type == 'edit'){
                     this.formModel.patchValue({
-                        department_id: department_id,
+                        department_id: this.editStatusDepartmentId,
                         department_name:this.departmentInfo['result']['department_name'],
                         department_number:this.departmentInfo['result']['department_number'],
                         department_shortcode:this.departmentInfo['result']['department_shortcode'],
@@ -222,6 +235,7 @@ export class SettingDepartmentnewComponent implements OnInit {
                 }
             });
     }
+
     closeSubmit(){
         this.formModel.patchValue({
             department_id: 0,
@@ -241,34 +255,46 @@ export class SettingDepartmentnewComponent implements OnInit {
      * @param type
      */
     deleteDepartment(type:any){
+        if(this.isStatus == 0 && type == 'id'){
+            return false;
+        }
         if(this.globalService.demoAlert('','')){
             return false;
         }
+
         let msg = '';
-
-        let is_select = 0;
         let department_id : string = '';
-        this.selects.forEach((val, idx, array) => {
-            if(val == true){
-                department_id += idx+',';
-                is_select += 1;
-            }
-        });
+        if(type == 'id'){
+            department_id = this.editStatusDepartmentId;
+        } else if(type == 'all'){
+            let is_select = 0;
 
-        if(is_select < 1){
-            msg = '请确认已选中需要删除的信息！';
-            alert(msg);
-            return false;
+            this.selects.forEach((val, idx, array) => {
+                if(val == true){
+                    department_id += idx+',';
+                    is_select += 1;
+                }
+            });
+
+            if(is_select < 1){
+                msg = '请确认已选中需要删除的信息！';
+                alert(msg);
+                return false;
+            }
         }
-        if(type == 'id' && is_select > 1){
-            msg = '此按钮只能删除指定的一行信息！';
-            alert(msg);
-            return false;
-        }else{
-            msg = '您确定要删除该信息吗？';
-        }
+        msg = '您确定要删除该信息吗？';
         if(confirm(msg)) {
-            let url = this.globalService.getDomain()+'/api/v1/deleteDepartmentById?department_id=' + department_id + '&type=all&sid=' + this.cookieStore.getCookie('sid');
+
+            let depart = '';
+            this.select_department_ids.forEach((val, idx, array) => {
+                if(val == true) {
+                    depart += idx + ',';
+                }
+            });
+            let url = this.globalService.getDomain()+'/api/v1/deleteDepartmentById?department_id=' +department_id + '&ids='+depart+'&type='+type+'&sid=' + this.cookieStore.getCookie('sid');
+            if(this.keyword.trim() != ''){
+                url += '&keyword='+this.keyword.trim();
+            }
             this.http.delete(url)
                 .map((res) => res.json())
                 .subscribe((data) => {
@@ -297,6 +323,13 @@ export class SettingDepartmentnewComponent implements OnInit {
             alert('请填写部门名称！');
             return false;
         }
+
+        let depart = '';
+        this.select_department_ids.forEach((val, idx, array) => {
+            if(val == true) {
+                depart += idx + ',';
+            }
+        });
         this.http.post(this.globalService.getDomain()+'/api/v1/addDepartment',{
             'department_id':this.formModel.value['department_id'],
             'department_name':this.formModel.value['department_name'],
@@ -309,6 +342,8 @@ export class SettingDepartmentnewComponent implements OnInit {
             'department_fax':this.formModel.value['department_fax'],
             'department_remark':this.formModel.value['department_remark'],
             'department_status':1,
+            'keyword':this.keyword,
+            'ids':depart,
             'u_id':this.cookieStore.getCookie('uid'),
             'sid':this.cookieStore.getCookie('sid')
         }).subscribe(
@@ -367,16 +402,43 @@ export class SettingDepartmentnewComponent implements OnInit {
     isStatusShow(department_id:any,department_status:any){
         this.editStatusDepartmentId = department_id;
         this.isStatus = department_status;
+        console.log('this.editStatusDepartmentId');
+        console.log(this.editStatusDepartmentId);
     }
 
     /**
      * 修改状态
      * @param status
+     * type   all 批量   id  单条操作
      */
-    editStatus(status:any){
+    editStatus(status:any,type:any){
+        let department_id = '';
+        if(type == 'all'){
+            console.log('this.selects');
+            console.log(this.selects);
+            this.selects.forEach((val, idx, array) => {
+                if(val == true){
+                    department_id += idx+',';
+                }
+            });
+        }else{
+            department_id = this.editStatusDepartmentId;
+        }
+        console.log('epartment_id:-----');
+        console.log(department_id);
+
+        let depart = '';
+        this.select_department_ids.forEach((val, idx, array) => {
+            if(val == true) {
+                depart += idx + ',';
+            }
+        });
         this.http.post(this.globalService.getDomain()+'/api/v1/addDepartment',{
-            'department_id':this.editStatusDepartmentId,
+            'department_id':department_id,
             'department_status':status,
+            'type':type,
+            'keyword':this.keyword.trim(),
+            'ids':depart,
             'sid':this.cookieStore.getCookie('sid')
         }).subscribe(
             (data)=>{
@@ -398,6 +460,16 @@ export class SettingDepartmentnewComponent implements OnInit {
                 this.isStatus = 0;
             }
         );
+    }
+
+    /**
+     * 批量
+     */
+    showAllCheck(){
+        this.isAll = 1;
+
+        this.editStatusDepartmentId = 0;
+        this.isStatus = 0;
     }
 
     @ViewChild('lgModal') public lgModal:ModalDirective;
