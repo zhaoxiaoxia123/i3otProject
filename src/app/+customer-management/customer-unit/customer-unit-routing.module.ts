090db4e62fb33b12d85a6041e6c2d9434ed790e9
MuXPhoenix <3427870036@qq.com>
2018-01-31 15:47:49 +0800

    5522

---
+++ src/app/+customer-management/customer-unit/customer-unit-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {CustomerUnitComponent} from "./customer-unit.component";
+
+
+export const CustomerUnitRoutes: Routes = [{
+    path: '',
+    component: CustomerUnitComponent
+}];
+export const CustomerUnitRoutingModule = RouterModule.forChild(CustomerUnitRoutes);
---
+++ src/app/+customer-management/customer-unit/customer-unit.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerUnitRoutingModule } from './customer-unit-routing.module';
+import { CustomerUnitComponent } from './customer-unit.component';
+import {I3otpModule} from "../../shared/i3otp.module";
+import {I3otpEditorsModule} from "../../shared/forms/editors/i3otp-editors.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CookieService} from "angular2-cookie/core";
+import {CookieStoreService} from "../../shared/cookies/cookie-store.service";
+
+@NgModule({
+  imports: [
+    CommonModule,
+      I3otpModule,
+      I3otpEditorsModule,
+      FormsModule,
+      ReactiveFormsModule,
+    CustomerUnitRoutingModule
+  ],
+  declarations: [CustomerUnitComponent],
+    providers:[ CookieService,CookieStoreService ]
+})
+export class CustomerUnitModule { }
