77dac7943371524224c6e90ded4f00c9ea62d542
zxx <540578223@qq.com>
2018-03-09 14:54:53 +0800

    xg

--- src/app/+forms/add-inventory1/add-inventory1.component.ts
+++ src/app/+forms/add-inventory1/add-inventory1.component.ts
@@ -6,7 +6,7 @@ import {ActivatedRoute,Router} from '@angular/router';
 import {CookieStoreService} from '../../shared/cookies/cookie-store.service';
 import {GlobalService} from '../../core/global.service';
 import {NotificationService} from "../../shared/utils/notification.service";
-
+import {getProvince,getCity,getArea} from '../../shared/common/area';
 // @FadeInTop()
 @Component({
   selector: 'app-add-inventory1',
@@ -22,6 +22,15 @@ export class AddInventory1Component implements OnInit {
   //默认选中的值
   storehouse_status_default : number;
   u_id_default : number;
+
+  //地址
+  province : string[] = [];
+  city : string[] = [];
+  area : string[] = [];
+  address1_default : number;
+  address2_default : number;
+  address3_default : number;
+
   rollback_url : string = '/forms/inventory1';
   constructor(
       fb:FormBuilder,
@@ -35,6 +44,7 @@ export class AddInventory1Component implements OnInit {
     let nav = '{"title":"添加仓库","url":"/forms/inventory1/0","class_":"active"}';
     this.globalService.navEventEmitter.emit(nav);
 
+    this.province = getProvince(); //住址
     this.formModel = fb.group({
       storehouse_id:[''],
       storehouse_name:['',[Validators.required,Validators.minLength(1)]],
@@ -45,7 +55,11 @@ export class AddInventory1Component implements OnInit {
       storehouse_notes:[''],
       storehouse_number:[''],
       storehouse_address:[''],
-      storehouse_shortcode:['']
+      storehouse_shortcode:[''],
+      address1:[''],
+      address2:[''],
+      address3:[''],
+
     });
   }
 
@@ -62,6 +76,17 @@ export class AddInventory1Component implements OnInit {
     this.getStorehouseDefault();
   }
 
+  getCity(){
+    let pro = this.formModel.value['address1'];
+    this.city = getCity(pro);
+    this.area = [];
+  }
+  getArea(){
+    let pro = this.formModel.value['address1'];
+    let city = this.formModel.value['address2'];
+    this.area = getArea(pro,city);
+    // console.log(this.area);
+  }
   /**
    * 获取添加订单的默认参数
    */
@@ -70,19 +95,20 @@ export class AddInventory1Component implements OnInit {
         .map((res)=>res.json())
         .subscribe((data)=>{
           this.storehouseList = data;
-        });
-    setTimeout(() => {
-      console.log(this.storehouseList);
-      if(this.storehouseList['status'] == 202){
-        alert(this.storehouseList['msg'] );
-        this.cookieStore.removeAll(this.rollback_url);
-        this.router.navigate(['/auth/login']);
-      }
-      if(this.storehouse_id == 0) {
-        this.storehouse_status_default = 1;
-        this.u_id_default = this.storehouseList['result']['userList'].length >= 1 ? this.storehouseList['result']['userList'][0]['id'] : 0;
-      }
-    }, 300);
+          console.log(this.storehouseList);
+          if(this.storehouseList['status'] == 202){
+            alert(this.storehouseList['msg'] );
+            this.cookieStore.removeAll(this.rollback_url);
+            this.router.navigate(['/auth/login']);
+          }
+          if(this.storehouse_id == 0) {
+            this.address1_default = 0;
+            this.address2_default = 0;
+            this.address3_default = 0;
+            this.storehouse_status_default = 1;
+            this.u_id_default = this.storehouseList['result']['userList'].length >= 1 ? this.storehouseList['result']['userList'][0]['id'] : 0;
+          }
+      });
   }
 
   getStorehouseInfo(storehouse_id:number){
@@ -90,23 +116,29 @@ export class AddInventory1Component implements OnInit {
         .map((res)=>res.json())
         .subscribe((data)=>{
           this.storehouse_info = data;
-        });
-
-    setTimeout(() => {
-      console.log(this.storehouse_info);
-      this.formModel.patchValue({
-        storehouse_id:this.storehouse_info['result']['storehouse_id'],
-        storehouse_name:this.storehouse_info['result']['storehouse_name'],
-        // storehouse_total_quantity:this.storehouse_info['result']['storehouse_total_quantity'],
-        storehouse_status:this.storehouse_info['result']['storehouse_status'],
-        u_id:this.storehouse_info['result']['u_id'],
-        storehouse_phone:this.storehouse_info['result']['storehouse_phone'],
-        storehouse_notes:this.storehouse_info['result']['storehouse_notes'],
-        storehouse_number:this.storehouse_info['result']['storehouse_number'],
-        storehouse_address:this.storehouse_info['result']['storehouse_address'],
-        storehouse_shortcode:this.storehouse_info['result']['storehouse_shortcode'],
+          console.log(this.storehouse_info);
+          this.formModel.patchValue({
+            storehouse_id:this.storehouse_info['result']['storehouse_id'],
+            storehouse_name:this.storehouse_info['result']['storehouse_name'],
+            // storehouse_total_quantity:this.storehouse_info['result']['storehouse_total_quantity'],
+            storehouse_status:this.storehouse_info['result']['storehouse_status'],
+            u_id:this.storehouse_info['result']['u_id'],
+            storehouse_phone:this.storehouse_info['result']['storehouse_phone'],
+            storehouse_notes:this.storehouse_info['result']['storehouse_notes'],
+            storehouse_number:this.storehouse_info['result']['storehouse_number'],
+            storehouse_address:this.storehouse_info['result']['storehouse_address'],
+            storehouse_shortcode:this.storehouse_info['result']['storehouse_shortcode'],
+            address1:this.storehouse_info['result']['address1'],
+            address2:this.storehouse_info['result']['address2'],
+            address3:this.storehouse_info['result']['address3'],
+          });
+          if(this.storehouse_info['result']['address1'] != 0){
+            this.getCity();
+          }
+          if(this.storehouse_info['result']['address2'] != 0){
+            this.getArea();
+          }
       });
-    }, 500);
   }
 
   onSubmit(){
@@ -124,7 +156,7 @@ export class AddInventory1Component implements OnInit {
       'storehouse_phone':this.formModel.value['storehouse_phone'],
       'storehouse_notes':this.formModel.value['storehouse_notes'],
       'storehouse_number':this.formModel.value['storehouse_number'],
-      'storehouse_address':this.formModel.value['storehouse_address'],
+      'storehouse_address':this.formModel.value['address1']+','+this.formModel.value['address2'] +','+ this.formModel.value['address3']+','+this.formModel.value['storehouse_address'],
       'storehouse_shortcode':this.formModel.value['storehouse_shortcode'],
       'sid':this.cookieStore.getCookie('sid')
     }).subscribe((data)=>{
