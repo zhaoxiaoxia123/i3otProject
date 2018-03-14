d4509c2f87c24213ca5d4eb1d0a82b631d940f09
zxx <540578223@qq.com>
2018-03-14 15:14:44 +0800

    xg

--- src/app/shared/layout/header/activities/activities.component.ts
+++ src/app/shared/layout/header/activities/activities.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ElementRef, Renderer, OnDestroy} from '@angular/core';
 import {ActivitiesService} from "./activities.service";
+import {Http} from "@angular/http";
+import {CookieStoreService} from "../../../cookies/cookie-store.service";
+import {GlobalService} from "../../../../core/global.service";
 
 declare var $: any;
 
@@ -15,25 +18,36 @@ export class ActivitiesComponent implements OnInit, OnDestroy {
   activities:any;
   currentActivity: any;
   loading: boolean;
-
+  uid:any;
   constructor(
-    private el:ElementRef,
-    private renderer: Renderer,
-    private activitiesService:ActivitiesService,
+      private http:Http,
+      private cookieStore:CookieStoreService,
+      private globalService:GlobalService,
+      private el:ElementRef,
+      private renderer: Renderer,
+    // private activitiesService:ActivitiesService,
     ) {
     this.active = false;
     this.loading = false;
     this.activities = [];
     this.count = 0;
     this.lastUpdate = new Date();
+    this.uid = this.cookieStore.getCookie('uid');
   }
 
   ngOnInit() {
-    this.activitiesService.getActivities().subscribe(data=> {
-      this.activities = data;
-      this.count = data.reduce((sum, it)=> sum + it.data.length, 0);
-      this.currentActivity = data[0];
-    });
+    // this.activitiesService.getActivities().subscribe(data=> {
+    //   this.activities = data;
+    //   this.count = data.reduce((sum, it)=> sum + it.data.length, 0);
+    //   this.currentActivity = data[0];
+    // });
+
+    // this.http.get(this.globalService.getDomain() + '/api/v1/getMessageCount?to=' + this.uid+'&category=notice,warning,task')
+    //     .map((res) => res.json())
+    //     .subscribe((data) => {
+    //       this.count = data.result.notice.count + data.result.warning.count + data.result.task.count ;
+    //       console.log(this.count);
+    //     });
   }
 
   setActivity(activity){
@@ -42,12 +56,21 @@ export class ActivitiesComponent implements OnInit, OnDestroy {
 
   private documentSub: any;
   onToggle() {
+
+    // this.http.get(this.globalService.getDomain() + '/api/v1/getNewMessages?to=' + this.uid+'&category=notice,warning,task')
+    //     .map((res) => res.json())
+    //     .subscribe((data) => {
+    //       this.activities = data;
+    //       this.currentActivity = data.result.notice.data[0];
+    //       console.log('this.activities:------');
+    //       console.log(this.activities);
+    //       console.log(this.currentActivity);
+    //     });
+
     let dropdown = $('.ajax-dropdown', this.el.nativeElement);
     this.active = !this.active;
     if (this.active) {
       dropdown.fadeIn()
-
-
       this.documentSub = this.renderer.listenGlobal('document', 'mouseup', (event) => {
         if (!this.el.nativeElement.contains(event.target)) {
           dropdown.fadeOut();
