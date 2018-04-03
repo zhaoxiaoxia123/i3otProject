5080978a17b2ba4d3a5d2a2fd7bfedceb6461e2b
zhaoxiaoxia123 <540578223@qq.com>
2018-04-03 16:30:12 +0800

    xg

--- todo-mission/todo-mission-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {TodoMissionComponent} from "./todo-mission.component";
-
-export const todoMissionRoutes: Routes = [{
-    path: '',
-    component: TodoMissionComponent
-}];
-export const TodoMissionRoutingModule = RouterModule.forChild(todoMissionRoutes);
