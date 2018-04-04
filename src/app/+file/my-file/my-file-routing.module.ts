4d94ec4a6d91b4a6bc729f61c1d9f61ef0823de2
zhaoxiaoxia123 <540578223@qq.com>
2018-04-04 11:52:53 +0800

    xg

---
+++ src/app/+file/my-file/my-file-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {MyFileComponent} from "./my-file.component";
+
+export const MyFileRoutes: Routes = [{
+    path: '',
+    component: MyFileComponent
+}];
+export const MyFileRoutingModule = RouterModule.forChild(MyFileRoutes);
---
+++ src/app/+mail/mail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {routing} from "./mail-routing.module";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+      routing
+  ],
+  declarations: []
+})
+export class MailModule { }
