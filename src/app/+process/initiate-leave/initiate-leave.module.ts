9d3c8942f6d082b6147c135f9793143c09c9b742
zhaoxiaoxia123 <540578223@qq.com>
2018-04-04 17:13:52 +0800

    xg

--- src/app/+process/initiate-leave/initiate-leave.module.ts
+++ src/app/+process/initiate-leave/initiate-leave.module.ts
@@ -5,14 +5,22 @@ import { InitiateLeaveRoutingModule } from './initiate-leave-routing.module';
 import { InitiateLeaveComponent } from './initiate-leave.component';
 import {I3otpModule} from "../../shared/i3otp.module";
 import {I3otpEditorsModule} from "../../shared/forms/editors/i3otp-editors.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {DpDatePickerModule} from "ng2-date-picker";
+import {CookieStoreService} from "../../shared/cookies/cookie-store.service";
+import {CookieService} from "angular2-cookie/core";
 
 @NgModule({
   imports: [
     CommonModule,
       I3otpModule,
       I3otpEditorsModule,
-    InitiateLeaveRoutingModule
-  ],
-  declarations: [InitiateLeaveComponent]
+    InitiateLeaveRoutingModule,
+  FormsModule,
+  ReactiveFormsModule,
+  DpDatePickerModule,
+],
+declarations: [InitiateLeaveComponent],
+    providers:[ CookieService,CookieStoreService ]
 })
 export class InitiateLeaveModule { }
