1fee28a881a5bacbeae7f3444ce2ac445f08ad5b
zhaoxiaoxia123 <540578223@qq.com>
2018-12-14 13:21:38 +0800

    xf

--- node_modules/@angular-redux/store/lib/src/ng-redux.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { NgZone } from '@angular/core';
-import { NgRedux } from './components/ng-redux';
-/** @hidden */
-export declare function _ngReduxFactory(ngZone: NgZone): NgRedux<{}>;
-export declare class NgReduxModule {
-}
